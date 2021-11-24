a0b64d0308e7434561acf86a60db6ee507dc6598
lucious20318 <88980279+lucious20318@users.noreply.github.com>
2021-11-24 12:03:23 +0530

    Update Library.java

--- Library.java
+++ Library.java
@@ -3,17 +3,17 @@ import java.io.*;
 
 public class Library 
 {
-    private static HashMap<Integer,ArrayList<Integer>> code_store;
-    private static HashMap<Integer,String> book_tit;
+    private static HashMap<Integer,ArrayList<Integer>> code_store = new HashMap<>();
+    private static HashMap<Integer,String> book_tit = new HashMap<>();
 
     public void set_code(int num,ArrayList<Integer> codes)
     {
         code_store.put(num, codes);
     }
 
-    public void set_tit()
+    public void set_tit(int num, String title)
     {
-
+        book_tit.put(num, title);
     }
 
     public HashMap get_code()
