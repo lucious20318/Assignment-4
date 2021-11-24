b1e6115aeaccfc15db2ec3aed0c2544d8f7002b0
lucious20318 <88980279+lucious20318@users.noreply.github.com>
2021-11-24 12:04:17 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,17 +9,19 @@ public class Main
     private static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) 
     {
-        System.out.print("Enter the number of books");
+        System.out.print("Enter the number of books :");
         int N = sc.nextInt();
         
-        System.out.print("Enter the number of racks");
+        System.out.print("Enter the number of racks :");
         int K = sc.nextInt();
 
-        float r = N/K;
+        float r = N%K;
         
-        public Library lib = new Library();
+        //System.out.println(r);
 
-        if(r != 0.0)
+        Library lib = new Library();
+
+        if(r != 0)
         {
             System.out.println("N is not a multiple of K,  Exiting.....");
             System.exit(0);
@@ -27,21 +29,27 @@ public class Main
 
         for(int i=0; i<N; i++)
         {
-            System.out.print("Enter the title of the book no. " + i + " :");
+            int j = i+1;
+
+            System.out.print("Enter the title of the book no. " + j + " :");
             String title = scan.next();
 
-            System.out.print("Enter the ISBN of the book no. " + i + " :");
+            System.out.print("Enter the ISBN of the book no. " + j + " :");
             int isbn = sc.nextInt();
 
-            System.out.print("Enter the Barcode of the book no. " + i + " :");
+            System.out.print("Enter the Barcode of the book no. " + j + " :");
             int barcode = sc.nextInt();
 
             ArrayList<Integer> codes = new ArrayList<>();
             codes.add(isbn);
             codes.add(barcode);
-            
 
+            lib.set_code(j, codes);
 
+            lib.set_tit(j, title);
+
+            System.out.println();
         }
+
     }
 }
